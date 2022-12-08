68b3f522e25f5bf010fc31b3dd862cbdb33c2f77
Daniel Mungai <danitomonga@gmail.com>
2022-12-08 19:05:19 +0300

    users database

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.users.routes import user_v2
-
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from config import DevelopmentConfig
 
 app = Flask(__name__)
+CORS(app)
+app.config.from_object(DevelopmentConfig)
+app.config['JWT_SECRET_KEY'] = 'BLAH'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
+jwt = JWTManager(app)
 app.register_blueprint(user_v2)
 app.debug = True
 
 
-
+from app.users import routes, model
 
 
