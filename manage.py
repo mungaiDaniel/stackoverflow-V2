89ea87044ddf1dc095203731bbb467d1896adb2d
mungaiDaniel <danitomonga@gmail.com>
2023-01-15 12:56:37 +0300

    my procfile

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.answers.model import Answer
 from app.database import MY_DATABASE
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
 
 
